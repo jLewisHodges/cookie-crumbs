6ef4196a69b9339b28f5382b70bb0fee50475399
Ihphillips <70382388+Ihphillips@users.noreply.github.com>
2020-09-14 16:31:56 -0400

    Create MenuItem.php

---
+++ CookieCrumbs/MenuItem.php
@@ -0,0 +1,31 @@
+<?php
+class MenuItem{
+    public $name;
+    public $price;
+    public $ingredients;
+
+    //Methods 
+
+    //setter and getter for name
+    function setName($name)
+    {
+    }
+    function getName($name)
+    {
+    }
+    //setter and getter for price
+    function setPrice($price)
+    {
+    }
+    function getPrice($price)
+    {
+    }
+    //setter and getter for ingredients
+    function setIngredients($ingredients)
+    {
+    }
+    function getIngredients($ingredients)
+    {
+    }
+}
+?>
