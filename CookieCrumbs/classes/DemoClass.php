8fd70ce48d5c2a281629360a49c3dd5d2c302171
Nick Cody <njcody@coastal.edu>
2020-09-16 12:55:38 -0400

    This is a demo and should not be merged.

---
+++ CookieCrumbs/classes/DemoClass.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    author: Nick Cody
+    DemoClass.php - this class is a demo and should not be merged
+*/
+    class DemoClass
+    {
+       //attributes
+       //methods
+    }   
+?>
