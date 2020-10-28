9da2f525d6c4ec0d1bc39f0cd975c295caf4cf59
Jorden Hodges <jlewishodges@gmail.com>
2020-10-28 18:52:32 -0400

    configured connection file for hostinger.com

--- CookieCrumbs/includes/connection.php
+++ CookieCrumbs/includes/connection.php
@@ -2,22 +2,14 @@
     class Connection
     {
         protected $dbServerName = "localhost";
-        protected $dbUserName = "id14705037_csci490team2";
-        protected $dbPassword = "Csci490team2!";
-        protected $dbName = "id14705037_cookiecrumbsdb";
+        protected $dbUserName = "u325576875_JordenHodges";
+        protected $dbPassword = "csci490Team2";
+        protected $dbName = "u325576875_CookieCrumbs";
         public $conn;
 
         function __construct()
         {
             $this->connect();
-            if(!$this->conn)
-            {
-                echo "Not Connected";
-            }
-            else
-            {
-                echo "connection successfully";
-            }
         }
 
         public function connect()
@@ -27,33 +19,6 @@
             {
                 die("Connection Failed: " . $this->conn->connect_error);
             } 
-            else
-            {
-                echo "connection sucessful";
-            }
-            $this->createTables();
-        }
-
-        private function createTables()
-        {
-            $usersTable = mysqli_query($this->conn, 'select 1 from users LIMIT 1');
-
-            if($usersTable != FALSE)
-            {
-                
-            }
-            else
-            {
-                $sql = "CREATE TABLE `users` ( `user_id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(20) NOT NULL , `last_name` VARCHAR(20) NOT NULL , `email_address` VARCHAR(50) NOT NULL , `password` VARCHAR(255) NOT NULL , PRIMARY KEY (`user_id`)) ENGINE = InnoDB;";
-
-                if($this->conn->query($sql) === TRUE)
-                {
-                    echo "table users created successfully";
-                }
-                else{
-                    echo "Error creating users table." . $this->conn->error;
-                }
-            }
         }
     }
 ?>
