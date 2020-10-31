d237f07ef470c31826579a8dc1313f6c342d8bc6
Fyjhrt <ipjuno99@gmail.com>
2020-10-31 17:49:11 -0400

    implenting_menuItem

--- CookieCrumbs/classes/MenuItem.php
+++ CookieCrumbs/classes/MenuItem.php
@@ -8,26 +8,39 @@ class MenuItem{
 
     //Methods 
 
-    //setter and getter for name
-    function setName($name){
-        $this->name = $name;
+    
+    function setItem_id($id){
+        $this->item_id = $id;
     }
-    function getName(){
-        return $this->name;
+    function getItem_id(){
+        return $this->item_id;
+    }
+    //setter and getter for name
+    function setItem_name($name){
+        $this->item_name = $name;
+    }
+    function getItem_name(){
+        return $this->item_name;
     }
     //setter and getter for price
-    function setPrice($price){
-        $this->price = $price;
+    function setItem_price($price){
+        $this->item_price = $price;
     }
-    function getPrice(){
-        return $this->price;
+    function getItem_price(){
+        return $this->item_price;
     }
     //setter and getter for ingredients
-    function setIngredients($ingredients){
-        $this->ingredient = $ingredients;
+    function setItem_description($description){
+        $this->item_description = $description;
     }
-    function getIngredients(){
-        return $this->ingredient;
+    function getItem_description(){
+        return $this->item_description;
+    }
+    function setItem_picture_name($picture){
+        $this->item_picture_name = $picture;
+    }
+    function getItem_picture_name(){
+        return $this->item_picture_name;
     }
 }
 ?>
