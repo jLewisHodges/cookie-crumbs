89e109fd0e70057cfcd8ad55164462ce9858679b
Fyjhrt <ipjuno99@gmail.com>
2020-11-12 16:04:55 -0500

    ok

---
+++ CookieCrumbs/manager_orders.php
@@ -0,0 +1,9 @@
+<?php
+$the_title = 'Order Tracker';
+include('header.php');?>
+    <!--HTML Code Here-->
+    <h2>Orders</h2>
+    <span style="border: 2px solid;"> hello</span><span style="border: 2px black;"> hello</span>
+    
+<?php
+include('footer.php');?>
