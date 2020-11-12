b1df51cf645d93ccf44d35e02210bb73d489070e
Fyjhrt <ipjuno99@gmail.com>
2020-11-12 18:01:41 -0500

    working on order stub

--- CookieCrumbs/manager_orders.php
+++ CookieCrumbs/manager_orders.php
@@ -3,7 +3,20 @@ $the_title = 'Order Tracker';
 include('header.php');?>
     <!--HTML Code Here-->
     <h2>Orders</h2>
-    <span style="border: 2px solid;"> hello</span><span style="border: 2px black;"> hello</span>
+    <div class="orders">
+    <div class="orderStub">
+    <p class="orderNum">42</p>
+    <p class="orderName">Ian</p>
+    <p class="orderMethod">Delivery</p>
+    <hr style="height:2px;border-width:0;color:black;background-color:black">
+    <ul class="orderItems">
+        <li>Steak</li>
+        <li>Reverse Seared NY Strip Steak</li>
+    </ul>
+        
+    </div>
+    </div>
+
     
 <?php
 include('footer.php');?>
