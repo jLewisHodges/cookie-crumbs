0eba793d859a61ede203ff54de754c1526a3f24b
Fyjhrt <ipjuno99@gmail.com>
2020-11-13 18:45:06 -0500

    added automatic order updating

--- CookieCrumbs/classes/getOrderAJAX.php
+++ CookieCrumbs/classes/getOrderAJAX.php
@@ -1,32 +1,18 @@
 <?php
 include_once(__DIR__."/../config.php");
 include_once(SITE_ROOT."/includes/connection.php");
-class getAllOrders
-    {
-        private $db;
-        private $innerHTML;
 
-        function __construct()
-        {
-            $this->db = new Connection();
-            $this->innerHTML = '';
-            $this->getMenuItems();
-
-        }
-
-        
-        private function getMenuItems()
-        {
+        $db = new Connection();
 
             $innerHTML='';
             $order_id="";
             $sql = "SELECT users.first_name, placed_orders.order_id, placed_orders.table_number, placed_orders.isDelivery FROM placed_orders JOIN users ON placed_orders.user_id = users.user_id";
 
             $sql2 = "SELECT menu_items.item_name FROM menu_items JOIN order_items on menu_items.item_id = order_items.item_id WHERE order_items.order_id = ?";
-            $stmt = $this->db->conn->prepare($sql2);
+            $stmt = $db->conn->prepare($sql2);
             $stmt->bind_param("s",$order_id);
 
-            if($result = $this->db->conn->query($sql))
+            if($result = $db->conn->query($sql))
             {
                 while($resultArray = $result->fetch_assoc())
                 {
@@ -52,14 +38,11 @@ class getAllOrders
                     }
                     $innerHTML = $innerHTML.'</ul></div>';
                 }
-                $this->innerHTML=$innerHTML;
+                //$this->innerHTML=$innerHTML;
             }
-        }
+        
 
-        public function getResult()
-        {
-            echo $this->innerHTML;
-        }
-    }
+
+            echo $innerHTML;
 ?>
 
--- CookieCrumbs/manager_orders.php
+++ CookieCrumbs/manager_orders.php
@@ -4,11 +4,30 @@ include('header.php');
 include_once('config.php');
 include_once(SITE_ROOT.'/classes/getAllOrders.php');
 $orders = new getAllOrders();
-//$orders->execute();
+//header('refresh:30');
 
 ?>
-    <!--HTML Code Here-->
-    <h2>Orders</h2>    
-    <?php echo $orders->getResult();?>
+<!--HTML Code Here-->
+<script>
+    function executeQuery() {
+        $.ajax({
+            url: 'classes/getOrderAJAX.php',
+            success: function(data) {
+             // do something with the return value here if you like
+             document.getElementById("orders").innerHTML = data;
+            }
+        });
+        setTimeout(executeQuery, 20000); // you could choose not to continue on failure...
+    }
+
+    $(document).ready(function() {
+        executeQuery();
+        setTimeout(executeQuery, 20000);
+    });
+</script>
+<h2>Orders</h2>
+<div id="orders">
+
+</div>
 <?php
 include('footer.php');?>
